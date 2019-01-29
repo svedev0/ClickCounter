e0b4d57781029cc2b2be5a5dd9d0f7e8c8f65b2b
MasterEman <emanuel@kulich.me>
2019-01-29 20:08:06 +0100

    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,6 @@
 package main;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -18,8 +17,8 @@ public class Main extends Application {
 	// This is just obvious
 	public static int windowWidth = 300;
 	public static int windowHeight = 300;
-
-	int clicks = 0;
+	
+	int clicks;
 	
 	public void start(Stage window) {
 		
@@ -29,20 +28,20 @@ public class Main extends Application {
 		Label clicksLabel = new Label("Number Of Clicks:");
 		Label clicksCounter = new Label();
 		
-		// Create incrementing button
-		Button clickIncrement = new Button("Click Here!");
-		
 		// Change text to clicks
 		clicksCounter.setText("" + clicks);
 		
+		// Create incrementing button
+		Button clickIncrement = new Button("Click Here!");
+		clickIncrement.setOnAction(e -> clicks++);
+		clickIncrement.setOnAction(e -> System.out.println(clicks));
+		
 		// Styling
-		clicksLabel.setStyle("-fx-font-size: 2.5em;"
-						   + "-fx-font-family: consolas;");
+		clicksLabel.setStyle("-fx-font-size: 2.5em; -fx-font-family: consolas;");
 		
-		clicksCounter.setStyle("-fx-font-size: 3em;"
-						     + "-fx-font-family: consolas;");
+		clicksCounter.setStyle("-fx-font-size: 3em; -fx-font-family: consolas;");
 		
-		clickIncrement.setStyle("");
+		clickIncrement.setStyle("-fx-font-size: 1.5em;");
 		
 		// Add all children
 		root.getChildren().add(clicksLabel);
