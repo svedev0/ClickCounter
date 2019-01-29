9fca1e2686db7ed7def2e23488ae48513b3f3391
MasterEman <emanuel@kulich.me>
2019-01-29 21:27:23 +0100

    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,7 @@
 package main;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -23,34 +20,11 @@ public class Main extends Application {
 	
 	public void start(Stage window) {
 		
-		// Main stackpane
 		StackPane root = new StackPane();
 		
-		// Main scene
 		Scene mainScene = new Scene(root, windowWidth, windowHeight);
 	
-		// Number of clicks
-		Label clicksLabel = new Label();
-		clicksLabel.setText("" + clicks);
 		
-		// Create incrementing button
-		Button clickIncrement = new Button("Click Here!");
-		clickIncrement.setOnAction(e -> clicks++);
-		clickIncrement.setOnAction(e -> System.out.println(clicks));
-		
-		root.getChildren().add(clicksLabel);
-		root.getChildren().add(clickIncrement);
-		
-		// Styling
-		StackPane.setAlignment(clickIncrement, Pos.CENTER);
-		clickIncrement.setStyle("-fx-background-radius: 20em; "
-							  + "-fx-min-width: 120px;"
-               				  + "-fx-min-height: 120px;"
-               				  + "-fx-max-width: 120px;"
-               				  + "-fx-max-height: 120px;");
-		
-		StackPane.setAlignment(clicksLabel, Pos.TOP_CENTER);
-		clicksLabel.setStyle("-fx-font-size: 3em;");
 		
 		// Window parameters
         window.getIcons().add(new Image("/icons/icon.png"));
