6833ea64c79ca55a1049518f163a4e8e420a1932
MasterEman <emanuel@kulich.me>
2019-01-29 21:33:48 +0100

    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,31 +6,48 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.awt.Button;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 public class Main extends Application {
-	
+
 	// This is just obvious
 	public static int windowWidth = 250;
 	public static int windowHeight = 210;
-	
+
 	int clicks;
-	
+
 	public static void main(String[] args) throws Exception {
 		launch(args);
 	}
-	
+
 	public void start(Stage window) {
-		
+
 		StackPane root = new StackPane();
-		
+
 		Scene mainScene = new Scene(root, windowWidth, windowHeight);
-	
-		
-		
+
+		Component button = new Button("Click Here!");
+
+		button.addMouseListener(new MouseAdapter() {
+
+			public void mouseClicked(MouseEvent e) {
+				
+				if (e.MOUSE_CLICKED) {
+					clicks++;
+				} else {
+					return;
+				}
+			}
+		});
+
 		// Window parameters
-        window.getIcons().add(new Image("/icons/icon.png"));
+		window.getIcons().add(new Image("/icons/icon.png"));
 		window.setTitle("Click Counter");
 		window.setResizable(false);
-        window.setScene(mainScene);
-        window.show();
-    }
+		window.setScene(mainScene);
+		window.show();
+	}
 }
