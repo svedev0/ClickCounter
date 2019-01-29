04cc33c3ea420a066d2f569bb0819572f3d6db48
MasterEman <emanuel@kulich.me>
2019-01-29 20:58:34 +0100

    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,56 +1,57 @@
 package main;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	
-	public static void main(String[] args) throws Exception {
-		launch(args);
-	}
-
 	// This is just obvious
-	public static int windowWidth = 300;
-	public static int windowHeight = 300;
+	public static int windowWidth = 250;
+	public static int windowHeight = 210;
 	
 	int clicks;
 	
+	public static void main(String[] args) throws Exception {
+		launch(args);
+	}
+	
 	public void start(Stage window) {
 		
-		VBox root = new VBox();
+		// Main stackpane
+		StackPane root = new StackPane();
 		
-		// Create labels
-		Label clicksLabel = new Label("Number Of Clicks:");
-		Label clicksCounter = new Label();
-		
-		// Change text to clicks
-		clicksCounter.setText("" + clicks);
+		// Main scene
+		Scene mainScene = new Scene(root, windowWidth, windowHeight);
+	
+		// Number of clicks
+		Label clicksLabel = new Label();
+		clicksLabel.setText("" + clicks);
 		
 		// Create incrementing button
 		Button clickIncrement = new Button("Click Here!");
 		clickIncrement.setOnAction(e -> clicks++);
 		clickIncrement.setOnAction(e -> System.out.println(clicks));
 		
-		// Styling
-		clicksLabel.setStyle("-fx-font-size: 2.5em; -fx-font-family: consolas;");
-		
-		clicksCounter.setStyle("-fx-font-size: 3em; -fx-font-family: consolas;");
-		
-		clickIncrement.setStyle("-fx-font-size: 1.5em;");
-		
-		// Add all children
 		root.getChildren().add(clicksLabel);
-		root.getChildren().add(clicksCounter);
 		root.getChildren().add(clickIncrement);
 		
-		// Main scene
-		Scene mainScene = new Scene(root, windowWidth, windowHeight);
-        
+		// Styling
+		StackPane.setAlignment(clickIncrement, Pos.BOTTOM_CENTER);
+		clickIncrement.setStyle("-fx-background-radius: 20em; "
+							  + "-fx-min-width: 120px;"
+               				  + "-fx-min-height: 120px;"
+               				  + "-fx-max-width: 120px;"
+               				  + "-fx-max-height: 120px;");
+		
+		StackPane.setAlignment(clicksLabel, Pos.TOP_CENTER);
+		clicksLabel.setStyle("-fx-font-size: 3em;");
+		
 		// Window parameters
         window.getIcons().add(new Image("/icons/icon.png"));
 		window.setTitle("Click Counter");
